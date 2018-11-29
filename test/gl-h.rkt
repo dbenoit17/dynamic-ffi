71b0b18322407ed7bb5cd43ab129933473471b87
dbenoit17 <dbenoit@redhat.com>
2018-11-28 20:14:43 -0500

    fix

--- test/gl-h.rkt
+++ test/gl-h.rkt
@@ -4,13 +4,6 @@
   racket/list
   "../dynamic-ffi/unsafe.rkt")
 
-
-(define string1 "hello")
-(define string2 " world")
-(define string1+string2
-  (string-append string1 string2))
-
-
 ;; Builds an auto-ffi
 (define-dynamic-ffi gl.h "/usr/include/GL/gl.h" "/usr/lib64/libGL")
 
--- test/glfw-h.rkt
+++ test/glfw-h.rkt
@@ -5,14 +5,8 @@
   "../dynamic-ffi/unsafe.rkt")
 
 
-(define string1 "hello")
-(define string2 " world")
-(define string1+string2
-  (string-append string1 string2))
-
-
 ;; Builds an auto-ffi
-(define-dynamic-ffi gl.h "/usr/lib64/libglfw" "/usr/include/GLFW/glfw3.h")
+(define-dynamic-ffi glfw.h "/usr/lib64/libglfw" "/usr/include/GLFW/glfw3.h")
 
 (provide (all-defined-out))
 
