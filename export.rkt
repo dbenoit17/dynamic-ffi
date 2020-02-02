6bb00d9d883ed4b351de9cfd31d62f829ccee338
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2020-02-02 11:20:32 -0800

    Feat: filter undefined symbols. Closes #31

--- export.rkt
+++ export.rkt
@@ -8,6 +8,7 @@
   racket/port
   racket/runtime-path
   racket/contract
+  (only-in ffi/unsafe ffi-lib _byte get-ffi-obj)
   (for-syntax racket/base)
   (prefix-in dffi: "meta.rkt"))
 
@@ -124,10 +125,12 @@
    (error "void only allowed as pointer or function return")]
   [else (error "unimplemented type")]))
 
+(define missing-symbol (gensym))
+
 ;; This is equivalent to build-ffi-obj-map in ffi.rkt,
 ;; except it produces racket source code instead
 ;; of runtime ffi objects.
-(define (format-ffi-obj-map ffi-data lib . headers)
+(define (format-ffi-obj-map ffi-data lib ffi-lib-obj . headers)
   (define pairs
     (for/list ([decl ffi-data])
       (define name (dffi:declaration-name decl))
@@ -140,10 +143,20 @@
                (format-dffi-obj type)]
               [(or (dffi:function-decl? decl)
                    (dffi:var-decl? decl))
-               (format "(get-ffi-obj '~a ~a\n    ~a \n    (warn-undefined-symbol '~a))" 
-                name lib (format-dffi-obj type) name)]
+               (and
+                ;; NOTE: use _byte as a dummy type
+                (not (eq? missing-symbol
+                          (get-ffi-obj (string->symbol name)
+                                       ffi-lib-obj
+                                       _byte
+                                       (λ () missing-symbol))))
+                (format "(get-ffi-obj '~a ~a\n    ~a \n    (warn-undefined-symbol '~a))"
+                        name lib (format-dffi-obj type) name))]
               [else
-                 (printf "warning: unimplemented delcaration type: ~a\n" decl)]))
+               (fprintf (current-error-port)
+                        "warning: unimplemented delcaration type: ~a\n"
+                        decl)
+               #f]))
       (cons (string->symbol name) ffi-obj)))
   (make-hash (filter (λ (x) (cdr x)) pairs)))
 
@@ -189,13 +202,14 @@
 ;; define-dynamic-ffi/cached in cached.rkt is an example of this.
 (define (create-static-ffi-generic dispatch ffi-data file ffi-name lib headers)
   (define library-name (format "~a-ffi-lib" ffi-name))
-  (define ffi-library 
+  (define-values (ffi-library ffi-lib-obj)
     (cond [(or (string? lib) (path? lib))
-           (format "(ffi-lib \"~a\")" lib)]
+           (values (format "(ffi-lib \"~a\")" lib)
+                   (ffi-lib lib))]
           [(pair? lib)
-           (format "(ffi-lib \"~a\" ~a)" (car lib)  (format-string-list (cdr lib)))]))
-  (define ffi-map
-    (apply format-ffi-obj-map (cons ffi-data (cons library-name headers))))
+           (values (format "(ffi-lib \"~a\" ~a)" (car lib)  (format-string-list (cdr lib)))
+                   (ffi-lib (car lib) (cdr lib)))]))
+  (define ffi-map (apply format-ffi-obj-map ffi-data library-name ffi-lib-obj headers))
   (dispatch file ffi-name library-name ffi-library headers ffi-map))
 
 (define (create-mapped-static-ffi ffi-data file ffi-name lib . headers)
